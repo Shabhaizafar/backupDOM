93e5a569ab90e50c775ddbd9d750d4a2b8de4d02
Shabhaizafar <zafarshabhai03@gmail.com>
2024-11-07 07:38:32 +0530

    queryselector

---
+++ DOM2.js
@@ -0,0 +1,24 @@
+// How to Access HTML Element Using JS : 
+// 1) getElementById : 
+// 2) getElementByClassName : 
+// 3) getElementByTagName : 
+
+// 4) querySelector :  we can Access any Single Element.
+
+// console.log(document.querySelector('h1')); //Tag Name
+// console.log(document.querySelector('p')); //Tag Name  
+// console.log(document.querySelector('.class1')); //class Name
+// console.log(document.querySelector('pre.class1'));// class + Tag name
+// console.log(document.querySelector('#heading2'));  // id
+
+// 5) querySelectorAll() : 
+// console.log(document.querySelectorAll('p'));//TagName
+// console.log(document.querySelectorAll('p')[1]);//TagName
+// console.log(document.querySelectorAll('.class1'));
+// console.log(document.querySelectorAll('.class1')[0]);
+// console.log(document.querySelectorAll('.class1')[1]);
+
+
+
+// index : 
+// console.log(document.querySelectorAll('#heading2')[0]);
